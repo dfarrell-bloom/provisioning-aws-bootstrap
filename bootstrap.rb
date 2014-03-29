3832a796664256aef0843c3b998c9d05c644deb7
Dan Farrell <dfarrell@bloomhealthco.com>
2014-03-28 21:55:04 -0500

    getting ready to download provisioning repo

--- bootstrap.rb
+++ bootstrap.rb
@@ -5,10 +5,11 @@ require 'aws-sdk'
 
 AWS.config(:credential_provider => AWS::Core::CredentialProviders::EC2Provider.new)
 
-provisioning_bucket = ARGV[0]
+s3bucket = ARGV[0]
+s3object = ARGV[1]
 
-s3 = AWS::S3.new( )
+s3 = AWS::S3.new()
 
-s3.buckets[ provisioning_bucket ].objects.each do |obj|
-    puts obj.key
+popen( "tar xvjp -C /opt/provisioning.git", "w" ) do |p|
+    s3.buckets[ s3bucket ].objects[ s3object ].write p:w
 end
